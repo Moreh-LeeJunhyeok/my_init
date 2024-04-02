2c461321426dd4b345e9fc7fec6a7a98e0ff087c
junhyeok.lee <junhyeok.lee@moreh.io>
2024-04-02 09:47:54 +0900

    misc

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# install packages
 sudo apt update
 sudo apt install build-essential -y
+apt install tmux -y
+
+# install zsh
 conda init bash
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 zsh
@@ -11,13 +15,16 @@ conda init zsh && source ~/.zshrc
 echo "changeps1: false" |& tee ~/.condarc
 conda activate moreh
 
+# install and set git lfs
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
 git config --global user.email "junhyeok.lee@moreh.io"
 git config --global user.name "junhyeok.lee"
 
+# install important python packages
 pip install -U -r $SCRIPT_DIR/requirements.txt
 
+# install moreh packages
 cd ~/
 conda activate moreh
 mkdir repos&& cd repos
@@ -27,4 +34,5 @@ git clone https://github.com/moreh-dev/moreh-datasets.git
 cd ~/repos/large-models && git lfs install && pip install -e .
 cd ~/repos/moreh-datasets && git lfs install && make install
 
+# re-install moai
 update-moreh
